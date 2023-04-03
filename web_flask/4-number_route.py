416ec521ffcc6c2ebe56cb0e355e5f6a62b11988
Tnozone <maxnocquet@gmail.com>
2023-04-03 04:54:04 -0700

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """returns Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbhn():
+    """displays HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def ctext(text):
+    """display C url"""
+    return "c {}".format(text.replace('_', ' ')).capitalize()
+
+
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythontext(text):
+    """display Python url"""
+    return "python {}".format(text.replace('_', ' ')).capitalize()
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def number(n):
+    """display n if int"""
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
