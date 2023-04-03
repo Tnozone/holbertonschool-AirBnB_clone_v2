80f82236aecaa6b22a674f92afeb6cb32b7259a4
Tnozone <maxnocquet@gmail.com>
2023-04-03 04:15:56 -0700

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """returns Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbhn():
+    """displays HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def ctext():
+    """display C followed by value"""
+    return "c {}".format(text.replace('_', ' ')).capitalize()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
